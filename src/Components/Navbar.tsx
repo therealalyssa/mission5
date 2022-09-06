a7781bb39c0ab3a1e823590a8d4ba58c0c59bdf9
Ranju Raveendran <ranju.raveendran@yoobeecolleges.com>
2022-09-06 12:44:01 +1200

    wip navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -7,11 +7,16 @@ function Navbar() {
   return (
     <div className='navbar'>
       <div className='links'>
-        <a href="/">Find a Vehicle</a>
+        {/* <a href="/">Find a Vehicle</a>
         <a href="/">How to Buy</a>
         <a href="/">Sell your Car</a>
         <a href="/">Finance</a>
-        <a href="/">Insurance</a>
+        <a href="/">Insurance</a> */}
+        <div>Find a Vehicle</div>
+        <div>How to Buy</div>
+        <div>Sell your Car</div>
+        <div>Finance</div>
+        <div>Insurance</div>
       </div>
       <div className='searchContact'>
         <form className='search'>
@@ -20,7 +25,7 @@ function Navbar() {
           </button>
           <input className='search-input' type='text' id='search' placeholder='search'></input>
         </form>
-        <a href="/"> Contact</a>
+        <div className='contact'> Contact</div>
       </div>
     </div>
   )
