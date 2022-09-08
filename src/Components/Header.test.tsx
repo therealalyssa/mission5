e33aac717453ab3e2c718be2c94680f37689ea65
ranjurave <ranjurave@gmail.com>
2022-09-08 22:03:21 +1200

    test6

--- src/Components/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen, cleanup} from '@testing-library/react';
-import Header from './Header';
-
-test('should render header', () =>{
-    render(<Header></Header>);
-    const todoElement = screen.getByTestId('header-1');
-    expect(todoElement).toBeInTheDocument();
-})
-export{}
