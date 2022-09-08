b9cd89e35328c304c12d54f5708119efcb537d21
Alyssa Barrett <cicibarrett@gmail.com>
2022-09-09 00:26:03 +1200

    page fixes

--- src/Components/Options.tsx
+++ src/Components/Options.tsx
@@ -5,7 +5,7 @@ import "../Styles/Options.css";
 function Options() {
   const tick = require("../images/Tick Icon.png");
   const cross = require("../images/Cross Icon.png");
-  // const download=require("../images/Status=Default.png")
+  const download=require("../images/Status=Default.png")
   return (
     <div className="options">
       <table>
@@ -201,8 +201,9 @@ function Options() {
           </td>
         </tr>
         <tr className="border-top">
+          <td></td>
           <td>
-            {/* <a id="test" href="https://content.tgstatic.co.nz/webassets/globalassets/terms-and-conditions/turners-third-party-fire-and-theft-policy-wording-0622.pdf" download="myimage" target="_blank" className="download2"><img src={String(download)} alt="tick" /></a> */}
+            <a id="test" href="https://content.tgstatic.co.nz/webassets/globalassets/terms-and-conditions/turners-third-party-fire-and-theft-policy-wording-0622.pdf" download="myimage" target="_blank" className="download2"><img src={String(download)} alt="tick" /></a>
             <div className='quote-buttons3'>
             <div className='green-button2'>
               Get a quote
@@ -210,7 +211,7 @@ function Options() {
             </div>
           </td>
           <td>
-            {/* <a id="test" href="https://content.tgstatic.co.nz/webassets/globalassets/terms-and-conditions/turners-third-party-fire-and-theft-policy-wording-0622.pdf" download="myimage" target="_blank"><img src={String(download)} alt="tick" /></a> */}
+            <a id="test" href="https://content.tgstatic.co.nz/webassets/globalassets/terms-and-conditions/turners-third-party-fire-and-theft-policy-wording-0622.pdf" download="myimage" target="_blank"><img src={String(download)} alt="tick" /></a>
             <div className='quote-buttons2'>
             <div className='green-button'>
               Get a quote
@@ -218,7 +219,7 @@ function Options() {
             </div>
           </td>
           <td>
-          {/* <td><a id="test" href="https://content.tgstatic.co.nz/webassets/globalassets/terms-and-conditions/turners-third-party-fire-and-theft-policy-wording-0622.pdf" download="myimage" target="_blank"><img src={String(download)} alt="tick" /></a> */}
+          <a id="test" href="https://content.tgstatic.co.nz/webassets/globalassets/terms-and-conditions/turners-third-party-fire-and-theft-policy-wording-0622.pdf" download="myimage" target="_blank"><img src={String(download)} alt="tick" /></a>
           <div className=' quote-buttons2'>
           <div className='green-button'>
             Get a quote
