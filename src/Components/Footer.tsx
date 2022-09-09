7baf4e8313b4d5062b68867a5a960d31935f2e18
Alyssa Barrett <cicibarrett@gmail.com>
2022-09-09 14:39:05 +1200

    fixing small bits

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,14 +2,20 @@
 import React from 'react'
 
 function Footer() {
+  const email = require("../images/Group 87.png");
+  const  insta= require("../images/Group 84.png");
+  const facebook = require("../images/Group 88.png");
+  const home = require("../images/Group 89.png");
+  const shield = require("../images/Shield Icon.png");
   return (
     <div className='header2'>
     <p>Ⓒ2021 Turners </p>
-    <h4>Branch Details</h4>
-    <h4>Facebook!!</h4>
-    <h4>Newsletter</h4>
-    <h4>Email Alerts!</h4>
-    <h4>Instagram</h4>
+    <h4> <img src={String(home)} alt="tick" /> Branch Details</h4>
+    <h4><img src={String(facebook)} alt="tick" />  Facebook</h4>
+    <h4><img src={String(email)} alt="tick" /> Newsletter</h4>
+    <h4><img src={String(email)} alt="tick" /> Email Alerts</h4>
+    <h4><img src={String(insta)} alt="tick" /> Instagram </h4>
+    <h4><img src={String(shield)} alt="tick" /> </h4>
   </div>
   )
 }
